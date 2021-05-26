709ff368c06dc5c6302415b2e24cf6f0c43467fb
Arteev Raina <arteevraina@gmail.com>
2021-05-26 19:40:07 +0530

    feat: added env variables & config file (#1)

---
+++ config.js
@@ -0,0 +1,9 @@
+// App Configuration keys here.
+require("dotenv").config();
+
+module.exports = {
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+const config = require("./config");
+
+// Checking if everything works fine.
+console.log(config.access_token);
+console.log(config.access_token_secret);
+console.log(config.consumer_key);
+console.log(config.consumer_secret);
