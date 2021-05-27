486cb8e99cedf8e047043eca0d70e7b5c176942d
Arteev Raina <arteevraina@gmail.com>
2021-05-27 09:41:18 +0530

    feat: bot now replying when someone mentions it

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
+// Package Imports.
+const Twit = require("twit");
+
+// Local Imports.
 const config = require("./config");
 
-// Checking if everything works fine.
-console.log(config.access_token);
-console.log(config.access_token_secret);
-console.log(config.consumer_key);
-console.log(config.consumer_secret);
+// Making a Twit object for connecting to the API.
+const T = new Twit(config);
+
+// Setting up a user stream
+const stream = T.stream("statuses/filter", { track: "@rainaarteev" });
+
+// Listening to Stream.
+stream.on("tweet", tweetEvent);
+
+// Function when tweet event is trigerred.
+function tweetEvent(tweet) {
+  console.log("Tweeted");
+  // Who is this in reply to?
+  let reply_to = tweet.in_reply_to_screen_name;
+
+  console.log(reply_to);
+  // Who sent the tweet?
+  let from = tweet.user.screen_name;
+
+  // Text of the tweet.
+  let text = tweet.text;
+
+  // Id for replying in thread.
+  let id = tweet.id_str;
+
+  if (reply_to === "RainaArteev") {
+    var newTweet = "@" + from + " thankyou for tweeting me!";
+    postTweet(newTweet, id);
+  }
+}
+
+// Function for Posting a tweet.
+function postTweet(txt, id) {
+  var tweet = {
+    status: txt,
+    in_reply_to_status_id: id,
+  };
+
+  // Post that tweet.
+  T.post("statuses/update", tweet, tweeted);
+
+  // Function to make sure tweet was sent.
+  function tweeted(err, reply) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Tweeted " + reply.text);
+    }
+  }
+}
