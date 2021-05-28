b9154c364a1740c2fb83b9a12c5e8e6d42ba06e7
aashimgarg <18bcs001@smvdu.ac.in>
2021-05-28 16:25:27 +0530

    added firebase for hosting

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaJGVG-jRhsDzSAJjfnp3hngW4VQ7rJ8U",
+  authDomain: "yourslots-b8b02.firebaseapp.com",
+  projectId: "yourslots-b8b02",
+  storageBucket: "yourslots-b8b02.appspot.com",
+  messagingSenderId: "681436241943",
+  appId: "1:681436241943:web:d184724c9c99c8493efc52",
+  measurementId: "G-ZP1YLEC378"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ 
